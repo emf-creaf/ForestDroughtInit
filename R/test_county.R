e55f924b0feeb3a271e46417857e61225dbf5a13
Miquel De Cáceres <miquelcaceres@gmail.com>
2025-07-21 16:00:52 +0200

    test two provinces

--- R/test_county.R
+++ R/test_county.R
@@ -2,7 +2,7 @@ source("R/init_province_medfateland.R")
 
 comarques <- sf::read_sf("~/OneDrive/EMF_datasets/PoliticalBoundaries/Catalunya/Comarques/comarques.shp")
 
-res <- 500
+res <- 200
 buffer_dist <- 50000
 emf_dataset_path <- "~/OneDrive/EMF_datasets/"
 test_plots <- TRUE
@@ -12,9 +12,8 @@ l <- init_province_medfateland(emf_dataset_path = emf_dataset_path,
                                target_polygon = ripolles,
                                res = res,
                                buffer_dist = buffer_dist,
-                               ifn_imputation_source = "IFN4",
-                               height_correction = TRUE)
+                               ifn_imputation_source = "IFN4")
 
-medfateland::plot_variable(l$sf, "elevation", r = l$r)
-medfateland::plot_variable(l$sf, "mean_tree_height", r = l$r)
-medfateland::plot_variable(l$sf, "basal_area", r = l$r)
+ggplot2::ggsave(filename = "plots/ripolles_elevation.png",medfateland::plot_variable(l$sf, "elevation", r = l$r),width = 5)
+ggplot2::ggsave(filename = "plots/ripolles_mean_tree_height.png",medfateland::plot_variable(l$sf, "mean_tree_height", r = l$r),width = 5)
+ggplot2::ggsave(filename = "plots/ripolles_basal_area.png",medfateland::plot_variable(l$sf, "basal_area", r = l$r),width = 5)
---
+++ R/test_two_provinces.R
@@ -0,0 +1,19 @@
+source("R/init_province_medfateland.R")
+
+res <- 2000
+buffer_dist <- 50000
+emf_dataset_path <- "~/OneDrive/EMF_datasets/"
+
+sf_all_provinces <- sf::read_sf(paste0(emf_dataset_path, "PoliticalBoundaries/Spain/Provincias_ETRS89_30N/Provincias_ETRS89_30N.gpkg"))
+sf_all_provinces <- sf::st_make_valid(sf_all_provinces)
+
+bcn_tar <- sf_all_provinces[c(8,43),]
+l <- init_province_medfateland(emf_dataset_path = emf_dataset_path,
+                               target_polygon = bcn_tar,
+                               res = res,
+                               buffer_dist = buffer_dist,
+                               ifn_imputation_source = "IFN4")
+
+ggplot2::ggsave(filename = "plots/bcntar_elevation.png",medfateland::plot_variable(l$sf, "elevation", r = l$r),width = 5)
+ggplot2::ggsave(filename = "plots/bcntar_mean_tree_height.png",medfateland::plot_variable(l$sf, "mean_tree_height", r = l$r),width = 5)
+ggplot2::ggsave(filename = "plots/bcntar_basal_area.png",medfateland::plot_variable(l$sf, "basal_area", r = l$r),width = 5)
