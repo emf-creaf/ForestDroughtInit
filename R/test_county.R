0e368fb632fb17f99527401dd831b7f1e54eee01
Miquel De Cáceres <miquelcaceres@gmail.com>
2025-07-21 15:35:42 +0200

    test ripolles

--- R/test_county.R
+++ R/test_county.R
@@ -2,12 +2,12 @@ source("R/init_province_medfateland.R")
 
 comarques <- sf::read_sf("~/OneDrive/EMF_datasets/PoliticalBoundaries/Catalunya/Comarques/comarques.shp")
 
-res <- 1000
-buffer_dist <- 50
+res <- 500
+buffer_dist <- 50000
 emf_dataset_path <- "~/OneDrive/EMF_datasets/"
 test_plots <- TRUE
 
-ripolles <- comarques[31,] 
+ripolles <- comarques[6,] 
 l <- init_province_medfateland(emf_dataset_path = emf_dataset_path,
                                target_polygon = ripolles,
                                res = res,
@@ -15,3 +15,6 @@ l <- init_province_medfateland(emf_dataset_path = emf_dataset_path,
                                ifn_imputation_source = "IFN4",
                                height_correction = TRUE)
 
+medfateland::plot_variable(l$sf, "elevation", r = l$r)
+medfateland::plot_variable(l$sf, "mean_tree_height", r = l$r)
+medfateland::plot_variable(l$sf, "basal_area", r = l$r)
